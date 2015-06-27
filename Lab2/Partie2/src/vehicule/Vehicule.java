a7d12dfedd99c2c653b886b1c7585adc9a30b9e7
charly <charly.simon.1@etsmtl.net>
2015-06-27 13:04:30 -0400

    Change Vehicule to use Tire class double "double getTireDiameter()" now deprecated

--- Lab2/Partie2/src/vehicule/Vehicule.java
+++ Lab2/Partie2/src/vehicule/Vehicule.java
@@ -4,20 +4,37 @@ public class Vehicule {
 	protected Transmission transmission = null;
 	protected Engine engine = null;
 	protected double tireSizeDiameter = 0.5;
+	protected Tire tire;
 	
 	public Vehicule(){
 		transmission = new Transmission();
 		engine = new Engine();
+		tire = new Tire();
 	}
+	
 	public Transmission getTransmission() {
 		return transmission;
 	}
+	
 	public Engine getEngine() {
 		return engine;
 	}
+	
+	/**
+	 * use instead {@link #getTire()} {@link Tire#getDiameter()} method.
+	 * @deprecated
+	 * @return tire diameter
+	 * 
+	 */
+	@Deprecated
 	public double getTireDiameter() {
 		return tireSizeDiameter;
 	}
+	
+	public Tire getTire() {
+		return tire;
+	}
+	
 	public void setTireSize(double tireSize) {
 		this.tireSizeDiameter = tireSize;
 	}	
