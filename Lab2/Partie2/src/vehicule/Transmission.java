956c33dadb18195bb6090493697f1acdf7af7afe
charly <charly.simon.1@etsmtl.net>
2015-06-27 13:00:30 -0400

    Modify Transmission to use Gear class Change "int getCurrentGear()" method. Now deprecated Change "void setCurrentGear(Gear)" method. Now deprecated

--- Lab2/Partie2/src/vehicule/Transmission.java
+++ Lab2/Partie2/src/vehicule/Transmission.java
@@ -10,22 +10,75 @@ public class Transmission {
 	public static final int REVERS_GEAR = -1;
 	
 	
-	protected int currentGear = NEUTRAL;
+	protected Gear currentGear;
+/*	protected int currentGear = NEUTRAL;*/
 	protected double differentialRatio = 4.0;
 	
-	public int getCurrentGear() {
-		return currentGear;
-	}
-	public void setCurrentGear(int gear) {
-		this.currentGear = gear;
+	
+	public Transmission() {
+		this.currentGear = Gear.NEUTRAL;
 	}
 	
+	/**
+	 * use instead {@link #getCurrentGearObj()} {@link Gear#getValue()} methods.
+	 * @deprecated
+	 * @return Gear value
+	 */
+	@Deprecated
+	public int getCurrentGear() {
+		return currentGear.getValue();
+	}
+	
+	/**
+	 * use instead {@link #setCurrentGear(Gear)} method.
+	 * @deprecated
+	 * @param gear
+	 */
+	@Deprecated
+	public void setCurrentGear(int gear) {
+		switch (currentGear.getValue()) {
+		case FIRST_GEAR:
+			this.currentGear = Gear.FIRST_GEAR;
+			break;
+		case SECOND_GEAR:
+			this.currentGear = Gear.SECOND_GEAR;
+			break;
+		case THIRD_GEAR:
+			this.currentGear = Gear.THIRD_GEAR;
+			break;
+		case FOURTH_GEAR:
+			this.currentGear = Gear.FOURTH_GEAR;
+			break;
+		case FIFTH_GEAR:
+			this.currentGear = Gear.FIFTH_GEAR;
+			break;
+		case NEUTRAL:
+			this.currentGear = Gear.NEUTRAL;
+			break;
+		case REVERS_GEAR:
+			this.currentGear = Gear.REVERS_GEAR;
+			break;
+		default:
+			this.currentGear = Gear.NEUTRAL;
+			break;
+		}
+	}
+
+	public Gear getCurrentGearObj() {
+		return currentGear;
+	}
+	
+	public void setCurrentGear(Gear currentGear) {
+		this.currentGear = currentGear;
+	}
+
 	public double getDifferentialRatio() {
 		return differentialRatio;
 	}
 	public void setDifferentialRatio(double differentialRatio) {
 		this.differentialRatio = differentialRatio;
 	}
+	
 	
 	
 }
