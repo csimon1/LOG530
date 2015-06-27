1128471e8cd5c3e0d901156d2f850b5727b901d4
charly <charly.simon.1@etsmtl.net>
2015-06-27 12:51:31 -0400

    Add class Tire

---
+++ Lab2/Partie2/src/vehicule/Tire.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package vehicule;
+
+/**
+ * @author MisterTim
+ *
+ */
+public class Tire {
+	protected double diameter;
+	
+	public Tire() {
+		diameter = 30;
+	}
+
+	public double getDiameter() {
+		return diameter;
+	}
+	
+	
+	
+	
+}
