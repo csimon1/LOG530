53b1ff8b2ecfe4c19a87e278cd90c18172d66ced
charly <charly.simon.1@etsmtl.net>
2015-06-27 13:16:40 -0400

    remove usage of deprecated methods

--- Lab2/Partie2/src/vehicule/Application.java
+++ Lab2/Partie2/src/vehicule/Application.java
@@ -3,9 +3,9 @@ package vehicule;
 public class Application {
 	public static void main(String[] args){
 		Vehicule v = new Vehicule();
-		v.setTireSize(0.5588);
+		v.setTire(new Tire(0.5588));/*v.setTireSize(0.5588);*/
 		v.getEngine().setRpm(3500);
-		v.getTransmission().setCurrentGear(Transmission.FIFTH_GEAR);
+		v.getTransmission().setCurrentGear(Gear.FIFTH_GEAR);/*v.getTransmission().setCurrentGear(Transmission.FIFTH_GEAR);*/
 		v.getTransmission().setDifferentialRatio(4.041);
 		Trip t = new Trip(100, v);		
 		System.out.println("Car traveling at : " + "->I need help to get this result<-");
